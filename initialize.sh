3545d2d8878f724d18e7fe1e04ef5ddc7d5fc7f2
KEKE <fengxxxke@gmail.com>
2022-07-20 06:39:12 +0000

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -49,6 +49,8 @@ export MSBuildSDKsPath=$DOTNET_ROOT/sdk/$(${DOTNET_ROOT}/dotnet --version)/Sdks
 export PATH="${PATH}:${DOTNET_ROOT}"
 EOF
 
+sudo snap install code --classic
+
 echo "Install GIT"
 sudo apt install git-all
 
